718f4ca327cab1f4ce2006c549b9c46db112c62d
Carlos Sandoval <carlosofscience@gmail.com>
2020-05-12 00:52:55 -0400

    Adding html template for dishes, and catalog styling

--- index.php
+++ index.php
@@ -1,11 +1,39 @@
+<?php
+  $dishes = [
+    [
+      "name" => "Twice-Baked Sweet Potatoes",
+      "description" => "We love a good twice-baked potato when we're craving something creamy, carb-y, and comforting. These twice-baked sweet potatoes exhibit all the same qualities, in addition to being super healthy—the perfect vegetarian dinner! ",
+      "prep_time" => "20 min"
+      // "ingredients" => [
+      // "6 large sweet potatoes, scrubbed clean",
+      // " 1 tbsp. extra-virgin olive oil",
+      // "Kosher salt",
+      // "1/2 c. milk",
+      // "1/4 c.sour cream",
+      // "4 tbsp. melted butter",
+      // "1 1/2 c. shredded Monterey Jack, divided",
+      // "1 c. black beans, rinsed and drained",
+      // "2 green onions, thinly sliced, plus more for garnish",
+      // "2 cloves garlic, minced",
+      // "1 c. quartered grape tomatoes ",
+      // "1 avocado, cubed",
+      // "1 small jalapeño, sliced into rounds",
+      // "1/4 c.crumbled queso fresco "
+      // ]
+    ]
+  ]
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>todo App</title>
+  <title>The Healty Catalog </title>
 </head>
 <body>
-  
+  <header>
+
+  </header>
 </body>
 </html>
